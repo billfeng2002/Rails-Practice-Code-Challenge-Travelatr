1c6050d70c30cf3c83d7d9fd31f9ed5fb6da4185
Bill <billfeng2002@gmail.com>
2021-07-07 09:33:21 -0700

    commit tree 2fbc9492c9f7e549a630cbe923fa7758269ac494

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/posts/:id/like', to: "posts#like", as: 'like_post'
+  resources :bloggers, only: [:new, :create, :show, :index]
+  resources :destinations, only: [:show, :index]
+  resources :posts, only: [:new, :create, :show, :index, :edit, :update]
 end
